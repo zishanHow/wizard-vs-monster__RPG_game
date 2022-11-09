938468a7876a56e179e4636a83c6f33eb988beb3
zishanHow <111298322+zishanHow@users.noreply.github.com>
2022-11-09 21:12:29 +0600

    render dice images via JS.
    depending on the random dice number i change the dice image between (1 - 6). and also make it more dynamic.

--- JSfile/index.js
+++ JSfile/index.js
@@ -1,13 +1,19 @@
 import characterData from '../JSfile/data.js'
 
-
-let diceCount = characterData.hero.diceCount
-
+let monsterArray = ["orc", "demon", "goblin"]
 
 
-let diceHtml = new Array(characterData.hero.diceCount).fill(0).map((dice)=>{
-    return dice = `<img class="dice" src="diceImg/dice1.png" alt="">`
-}).join('')
+
+
+document.getElementById('attack-button').addEventListener('click', attack)
+function attack(){
+    render()
+}
+
+
+
+
+
 
 function getDicePlaceHolderHtml(diceCount){
     return new Array(diceCount).fill(0).map(()=>{
@@ -17,14 +23,55 @@ function getDicePlaceHolderHtml(diceCount){
 
 
 
-document.getElementById('hero').innerHTML = `
-        <div class="character-card">
-            <h4 class="name"> ${characterData.hero.name} </h4>
-            <img class="avatar" src="${characterData.hero.avatar}" />
-            <div class="health">health: <b> ${characterData.hero.health} </b></div>
 
-            <div class="dice-container">
-                ${diceHtml}
-            </div>
-        </div>
-`
+
+
+// setting random number and getting dice roll array
+function getDiceRollArray(diceCount){
+    return new Array(diceCount).fill(0).map(() => 
+        Math.floor( Math.random() * 6 ) + 1
+    )
+}
+
+// creating a CLASS for "blueprint" our data.js object 
+class Character {
+    constructor(data){
+        // takes all the data from 'data'(parameter) object and set it to the 'this' object
+        Object.assign(this, data)
+    }
+
+    setDiceHtml(diceCount){
+        return getDiceRollArray(diceCount).map( (num) => {
+            return `<img class="dice" src="diceImg/dice${num}.png" alt="Your dice number ${num}">`
+        }).join('')
+    }
+
+    getCharacterHtml(){
+        // Object destructuring
+        const { name, avatar, health, diceCount } = this
+
+        const diceHtml = this.setDiceHtml(diceCount)
+        return `
+            <div class="character-card">
+                <h4 class="name"> ${name} </h4>
+                <img class="avatar" src="${avatar}" />
+                <div class="health">health: <b> ${health} </b></div>
+
+                <div class="dice-container">
+                    ${diceHtml}
+                </div>
+            </div
+        `
+    }
+}
+
+
+function render(){
+    document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
+    document.getElementById('monster').innerHTML = orc.getCharacterHtml()
+}
+
+const wizard = new Character(characterData.hero)
+const orc = new Character(characterData.orc)
+
+render()
