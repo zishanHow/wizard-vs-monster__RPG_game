103086fa999c65ba43e9b5ae1d7f658a2232fabd
zishanHow <111298322+zishanHow@users.noreply.github.com>
2022-11-09 19:26:24 +0600

    added dice image and find out how it works.

--- JSfile/index.js
+++ JSfile/index.js
@@ -1,3 +1,30 @@
 import characterData from '../JSfile/data.js'
 
-console.log(characterData)
+
+let diceCount = characterData.hero.diceCount
+
+
+
+let diceHtml = new Array(characterData.hero.diceCount).fill(0).map((dice)=>{
+    return dice = `<img class="dice" src="diceImg/dice1.png" alt="">`
+}).join('')
+
+function getDicePlaceHolderHtml(diceCount){
+    return new Array(diceCount).fill(0).map(()=>{
+        `<div class="placeholder-dice"></div>`
+    })
+}
+
+
+
+document.getElementById('hero').innerHTML = `
+        <div class="character-card">
+            <h4 class="name"> ${characterData.hero.name} </h4>
+            <img class="avatar" src="${characterData.hero.avatar}" />
+            <div class="health">health: <b> ${characterData.hero.health} </b></div>
+
+            <div class="dice-container">
+                ${diceHtml}
+            </div>
+        </div>
+`
